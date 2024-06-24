608607b4d73fae37efebb0c63b661dfaead0a7f4
jaeyeonjjang <wodus0936@gmail.com>
2024-06-24 21:46:44 +0900

    Initial commit

---
+++ src/main/java/com/woof/meongmeong/MeongmeongApplication.java
@@ -0,0 +1,13 @@
+package com.woof.meongmeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongmeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongmeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woof/meongmeong/MeongmeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woof.meongmeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongmeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
