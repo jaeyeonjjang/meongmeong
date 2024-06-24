4a86932628db238aa8207140e8a33e373a633f95
jaeyeonjjang <wodus0936@gmail.com>
2024-06-24 21:57:30 +0900

    깃 테스트

--- src/main/java/com/woof/meongmeong/MeongmeongApplication.java
+++ src/main/java/com/woof/meongmeong/MeongmeongApplication.java
@@ -2,12 +2,16 @@ package com.woof.meongmeong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MeongmeongApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeongmeongApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(MeongmeongApplication.class, args);
+
+        //승호야안뇽 ㅋ ㅋ ㅋ
+    }
 
 }
--- src/test/java/com/woof/meongmeong/MeongmeongApplicationTests.java
+++ src/test/java/com/woof/meongmeong/MeongmeongApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MeongmeongApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
